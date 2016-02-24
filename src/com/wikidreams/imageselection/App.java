20abf2c95cfb629c79dfee3430230a225be779ac
WikiDreams <danielsantosvieiramail@gmail.com>
2016-02-24 17:48:10 +0000

    update

--- src/com/wikidreams/imageselection/App.java
+++ src/com/wikidreams/imageselection/App.java
@@ -1,104 +1,48 @@
 package com.wikidreams.imageselection;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class App {
 
-	Rectangle captureRect;
-
-	App(final BufferedImage screen) {
-final BufferedImage screenCopy = new BufferedImage(
-				screen.getWidth(),
-				screen.getHeight(),
-				screen.getType());
-		final JLabel screenLabel = new JLabel(new ImageIcon(screenCopy));
-		JScrollPane screenScroll = new JScrollPane(screenLabel);
-
-		screenScroll.setPreferredSize(new Dimension(
-				(int)(screen.getWidth()/3),
-				(int)(screen.getHeight()/3)));
-
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.add(screenScroll, BorderLayout.CENTER);
-
-		final JLabel selectionLabel = new JLabel(
-				"Drag a rectangle in the screen shot!");
-		panel.add(selectionLabel, BorderLayout.SOUTH);
-
-		repaint(screen, screenCopy);
-		screenLabel.repaint();
-
-		screenLabel.addMouseMotionListener(new MouseMotionAdapter() {
-
-			Point start = new Point();
-
-			@Override
-			public void mouseMoved(MouseEvent me) {
-				start = me.getPoint();
-				repaint(screen, screenCopy);
-				selectionLabel.setText("Start Point: " + start);
-				screenLabel.repaint();
-			}
-
-			@Override
-			public void mouseDragged(MouseEvent me) {
-				Point end = me.getPoint();
-				captureRect = new Rectangle(start,
-						new Dimension(end.x-start.x, end.y-start.y));
-				repaint(screen, screenCopy);
-				screenLabel.repaint();
-				selectionLabel.setText("Rectangle: " + captureRect);
-			}
-		});
-
-		JOptionPane.showMessageDialog(null, panel);
-
-		System.out.println("Rectangle of interest: " + captureRect);
-	}
-
-	public void repaint(BufferedImage orig, BufferedImage copy) {
-		Graphics2D g = copy.createGraphics();
-		g.drawImage(orig,0,0, null);
-		if (captureRect!=null) {
-			g.setColor(Color.RED);
-			g.draw(captureRect);
-			g.setColor(new Color(255,255,255,150));
-			g.fill(captureRect);
-		}
-		g.dispose();
-	}
-
 	public static void main(String[] args) throws Exception {
-		/*
-		Robot robot = new Robot();
-		final Dimension screenSize = Toolkit.getDefaultToolkit().
-				getScreenSize();
-		final BufferedImage screen = robot.createScreenCapture(
-				new Rectangle(screenSize));
-
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new App(screen);
+				//new LabelImage();
+				JFrame frame = new JFrame("Window");
+				frame.setLayout(new BorderLayout());
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				MyPanel panel = new MyPanel();
+				frame.add(panel, BorderLayout.CENTER);
+				frame.setVisible(true);
+				frame.pack();
+				frame.addKeyListener(new KeyListener() {
+					@Override
+					public void keyTyped(KeyEvent e) {
+						if (e.getKeyChar() == 'd') {
+							panel.nextImage();	
+						}
+						if (e.getKeyChar() == 's') {
+							panel.saveCoordinatesFromSelectedRegion();	
+						}
+						if (e.getKeyChar() == 'c') {
+							panel.createImageFromSelectedRegion();	
+						}
+					}
+
+					@Override
+					public void keyReleased(KeyEvent e) {
+					}
+
+					@Override
+					public void keyPressed(KeyEvent e) {
+					}
+				});
 			}
 		});
-		*/
-		new Selection();
 	}
 }
---
+++ src/com/wikidreams/imageselection/MyPanel.java
@@ -0,0 +1,259 @@
+package com.wikidreams.imageselection;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MyPanel extends JPanel {
+
+	private File dir;
+	private ArrayList<File> result;
+
+	private JLabel screenLabel;
+	private Image image;
+	private BufferedImage screen;
+	private BufferedImage screenCopy;
+	private int currentSelectedImage;
+
+	private Rectangle captureRect;
+	private Rectangle captureRectCopy;
+
+	public MyPanel() {
+		openDialog();
+		this.captureRect = null;
+		this.captureRectCopy = null;
+
+		this.dir = new File("C:\\Development\\data\\images\\");
+		if (! this.dir.exists()) {
+			JOptionPane.showMessageDialog(null, "Select a folder with images.");
+			return;
+		}
+
+		this.result = new ArrayList<File>();
+		this.displayDirectoryContents(this.dir, this.result);
+
+		this.image = null;
+		try {
+			image = ImageIO.read(new File(this.result.get(0).getAbsolutePath()));
+			this.currentSelectedImage = 1;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+
+		this.screen = (BufferedImage) image;
+		this.screenCopy = new BufferedImage(this.screen.getWidth(), this.screen.getHeight(), this.screen.getType());
+
+		this.screenLabel = new JLabel(new ImageIcon(this.screenCopy));
+		this.screenLabel.setPreferredSize(new Dimension(this.screen.getWidth(), this.screen.getHeight()));
+
+		this.setLayout(new BorderLayout());
+		this.add(screenLabel, BorderLayout.CENTER);
+
+		final JLabel selectionLabel = new JLabel("Drag a rectangle in the image.");
+		this.add(selectionLabel, BorderLayout.SOUTH);
+
+		repaint(screen, screenCopy);
+		screenLabel.repaint();
+
+		screenLabel.addMouseMotionListener(new MouseMotionAdapter() {
+
+			Point start = new Point();			
+
+			@Override
+			public void mouseMoved(MouseEvent me) {
+				if (captureRect == null) {
+					start = me.getPoint();
+					repaint(screen, screenCopy);
+					selectionLabel.setText("Start Point: " + start);
+					screenLabel.repaint();
+				}
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent me) {
+				if (start != null) {
+					Point end = me.getPoint();
+					captureRect = new Rectangle(start, new Dimension(end.x-start.x, end.y-start.y));
+					repaint(screen, screenCopy);
+					screenLabel.repaint();
+					selectionLabel.setText("Rectangle: " + captureRect);
+					System.out.println(captureRect);
+				}
+			}
+		});
+
+		screenLabel.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent me) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent me) {
+				if (captureRect != null) {
+					Point start = me.getPoint();
+					captureRectCopy = new Rectangle(start, captureRect.getSize());
+					autoRepaint(screen, screenCopy);
+					screenLabel.repaint();
+				}
+			}
+		});
+	}
+
+	private void repaint(BufferedImage orig, BufferedImage copy) {
+		Graphics2D g = copy.createGraphics();
+		g.drawImage(orig,0,0, null);
+		if (captureRect!=null) {
+			g.setColor(Color.RED);
+			g.draw(captureRect);
+			g.setColor(new Color(255,255,255,150));
+			g.fill(captureRect);
+		}
+		g.dispose();
+	}
+
+	private void autoRepaint(BufferedImage orig, BufferedImage copy) {
+		Graphics2D g = copy.createGraphics();
+		g.drawImage(orig,0,0, null);
+		if (captureRectCopy!=null) {
+			g.setColor(Color.RED);
+			g.draw(captureRectCopy);
+			g.setColor(new Color(255,255,255,150));
+			g.fill(captureRectCopy);
+		}
+		g.dispose();
+	}
+
+	private void displayDirectoryContents(File dir, ArrayList<File> result) {
+		try {
+			File[] files = dir.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					//System.out.println("directory:" + file.getAbsolutePath());
+					displayDirectoryContents(file, result);
+				} else {
+					//System.out.println("     file:" + file.getAbsolutePath());
+					result.add(new File(file.getAbsolutePath()));
+				}		
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void nextImage() {
+		if (this.captureRect != null) {
+
+			if (this.currentSelectedImage != this.result.size()) {
+				try {
+					this.image = ImageIO.read(new File(this.result.get(this.currentSelectedImage).getAbsolutePath()));
+					this.currentSelectedImage +=1;
+					this.screen = (BufferedImage) this.image;
+					this.screenCopy = new BufferedImage(this.screen.getWidth(), this.screen.getHeight(), this.screen.getType());
+					this.screenLabel.setIcon(new ImageIcon(this.screenCopy));
+					System.out.println(captureRectCopy);
+					this.autoRepaint(this.screen, this.screenCopy);
+					this.screenLabel.repaint();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Drag a rectangle in the image.");
+		}
+	}
+
+
+	public void saveCoordinatesFromSelectedRegion() {
+		try {
+
+			String content = "";
+			if (this.captureRectCopy == null) {
+				content = captureRect.toString();
+			} else {
+				content = captureRectCopy.toString();
+			}
+
+			File file = new File("C:\\Development\\data\\images\\info.txt");
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+			JOptionPane.showMessageDialog(null, "File saved.");
+		} catch (HeadlessException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "The system cannot find the path specified");
+		}
+
+	}
+
+
+	public void createImageFromSelectedRegion() {
+		try {
+			BufferedImage selectedImage = this.screen.getSubimage(this.captureRect.x, this.captureRect.y, this.captureRect.width, this.captureRect.height);
+			ImageIO.write(selectedImage, "jpg", new File("c:\\output.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void openDialog() {
+		JFileChooser fc = new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int returnVal = fc.showOpenDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+		}
+	}
+
+	private void saveDialog() {
+		final JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showSaveDialog(this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+		}
+	}
+
+}
